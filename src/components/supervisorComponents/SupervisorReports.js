2c374c8a53c343e66e26367766cda89cfc9a4f89
JoeLamptey <rutherford.gh@gmail.com>
2019-05-12 18:30:13 +0300

    supervisor report

--- src/components/supervisorComponents/SupervisorReports.js
+++ src/components/supervisorComponents/SupervisorReports.js
@@ -1,27 +1,90 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
-class SupervisorReports extends Component{
-    render(){
-        return(
-            <div>
-                <h1>SupervisorReports Page</h1>                
-                <Typography paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-                        elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                        hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-                        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-                        Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-                        viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-                        Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-                        at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-                        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-                    </Typography>
-               
-            </div>
-        )
-    }
+import SupervisorCreateReport from './SupervisorCreateReport'
+import SupervisorViewReport from './SupervisorViewReport'
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
 }
 
-export default SupervisorReports
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    margin: '0% 15%',
+    backgroundImage: 'none !important',
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  button:{
+      marginTop: theme.spacing.unit,
+  }
+});
+
+class CarerReport extends Component {
+  state = {
+    value: 0,
+    notification: '',
+    newClient: {}
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  createClient = (e) =>{
+    console.log(e) 
+  }
+  
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default" >
+          <Tabs value={value} onChange={this.handleChange} textColor="primary">
+            <Tab label="Create Report" />
+            <Tab label="View Reports" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && 
+            <TabContainer>                
+                <SupervisorCreateReport createClient={this.createClient}/>
+            </TabContainer>
+        }
+        {value === 1 && 
+            <TabContainer>
+                <SupervisorViewReport />
+            </TabContainer>}
+      </div>
+    );
+  }
+}
+
+CarerReport.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CarerReport);
